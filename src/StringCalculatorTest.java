2684898af86c296213f5e06ca0d404ad0b2088c8
vaipan@mcb.mu <v$efgrthjy02P>
2024-01-01 18:35:33 +0530

    Adding simple unit test cases for number check.

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,8 +1,6 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Spy;
 
 public class StringCalculatorTest {
 
@@ -14,8 +12,16 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void emptyString() {
+    public void testEmptyString() {
         Assert.assertEquals(stringCalculator.Add(""), 0);
     }
 
+    @Test
+    public void testSingleNumber() {
+        Assert.assertEquals(stringCalculator.Add("1"), 1);
+    }
+    @Test
+    public void testMultipleNumber() {
+        Assert.assertEquals(stringCalculator.Add("1,2,3,4"), 10);
+    }
 }
